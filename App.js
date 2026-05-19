5099ced0966a429ebe67ef82e24afa5025d9e28a
vinisant0lopes-crypto <vinisant0lopes@gmail.com>
2026-05-19 17:57:58 -0300

    Alterações garfled

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ import Feather from '@expo/vector-icons/Feather';
 export default function App() {
   return (
     <View style={styles.container}>
+
       {/* Inicio da Header */}
 
       <View style ={styles.viewHeader}>
       
-        <Feather name="menu" size={24} color="black" />   
+        <Feather name="menu" size={24} color="white" />   
         <Text style ={styles.textHeader}> TECFILMES </Text>
         <TouchableOpacity>
           
@@ -21,12 +22,19 @@ export default function App() {
 
       <View style = {styles.containerSearch}>
       <TextInput 
-      placeholder='Digite o filme que deseja buscar'
+      placeholder='Digite o filme que deseja buscar' style={styles.inputSearch}
       ></TextInput>
+
+      <TouchableOpacity>
+      <Feather name="search" size={24} color="black"/>
+      </TouchableOpacity>
       </View>
 
       {/* Inicio do banner */}
     
+    <Text style={styles.textBanner}> Em cartaz </Text>
+
+    <Image source={require('./assets/garfeld.jpg')} style={styles.imageBanner}/>
 
     </View>
   );
@@ -58,7 +66,31 @@ const styles = StyleSheet.create({
   width:'90%',
   backgroundColor:'white',
   borderRadius:5,
-  padding:8
+  padding:8,
+  flexDirection:'row',
+  justifyContent:'space-between',
+  alignItems:'center'
+ },
+
+ inputSearch:{
+  height:40,
+  padding:8,
+  width:'100%'
+ },
+
+ imageBanner:{
+  width: 300,
+  height:400,
+  marginTop:15,
+  borderRadius:10,
+ },
+
+ textBanner:{
+  color:'white',
+  width:'90%',
+  fontSize:30,
+  marginTop:20,
+  fontWeight:'bold'
  }
 
 });
