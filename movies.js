66ab02fbde6288067b563ef1b44fa6b6fe98e8f5
vinisant0lopes-crypto <vinisant0lopes@gmail.com>
2026-05-26 18:00:01 -0300

    Filmes

---
+++ movies.js
@@ -0,0 +1,63 @@
+const DATA =[
+    {
+        id: 1,
+        nome:'F1',
+        nota:'7.6',
+        imagem:'./assets/f1_filmes.webp'
+    },
+    {
+        id: 2,
+        nome:'Titanic',
+        nota:'8.0',
+        imagem:'./assets/titanic_filme.jpg'
+    },
+    {
+        id: 3,
+        nome:'Viva A Vida É Uma Festa',
+        nota:'8.4',
+        imagem:'./assets/filme_dos_morto.png'
+    },
+    {
+        id: 4,
+        nome:'Cruella',
+        nota:'7.0',
+        imagem:'./assets/cruella_filme.jpg'
+    },
+    {
+        id: 5,
+        nome:'O Rei Leão',
+        nota:'8.5',
+        imagem:'./assets/rei_leao_filme.jpeg'
+    },
+    {
+        id: 6,
+        nome:'Vingadores: Guerra Infinita',
+        nota:'8.4',
+        imagem:'./assets/guerra_infinita.jpg'
+    },
+    {
+        id: 7,
+        nome:'Godzilla: King Of The Monsters',
+        nota:'6.0',
+        imagem:'./assets/godzilla_movie.png'
+    },
+    {
+        id: 8,
+        nome:'Barbie Em Um Mundo De Videogame',
+        nota:'5.0',
+        imagem:'./assets/barbie_videogame.jpg'
+    },
+    {
+        id: 9,
+        nome:'Gigante De Aço',
+        nota:'7.1',
+        imagem:'./assets/godzilla_vs_kong.jpg'
+    },
+    {
+        id: 10,
+        nome:'Gente Grande',
+        nota:'6.0',
+        imagem:'./assets/gente_grande.jpg'
+    }
+
+]
