32e26247d511e8eaf03b72b4a17692f2377a4a10
vinisant0lopes-crypto <vinisant0lopes@gmail.com>
2026-09-15 17:38:15 -0300

    Adicionado o stack

---
+++ src/Rotas/index.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+export default funcion Rotas(){
+
+
+    const Stack = createStackNavigator();
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                
+            </Stack.Navigator>
+
+        </NavigationContainer>
+
+
+    );
+}
---
+++ src/telas/Detalhes/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Detalhes(){
+
+    return(
+        <View>
+            <Text>Essa é minha tela de detalhes</Text>
+        </View>
+    )
+}
